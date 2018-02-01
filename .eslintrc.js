7a185c489e30e71b683add50e9c349177cf53c3d
Robert Luiz <Robert.tiweb@gmail.com>
2018-02-01 04:17:00 -0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": [
+          "node_modules",
+          "src"
+        ]
+      }
+    }
+  }
 };
