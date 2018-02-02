71b499a95b01d4b0c62c168ae19d12d58f6624ee
Robert Luiz <Robert.tiweb@gmail.com>
2018-02-02 01:45:25 -0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   "extends": "airbnb-base",
+  "plugins": [
+    "mocha"
+  ],
+  "rules": {
+    "mocha/no-exclusive-tests": "error"
+  },
   "settings": {
     "import/resolver": {
       "node": {
@@ -9,5 +15,24 @@ module.exports = {
         ]
       }
     }
-  }
+  },
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "no-unused-vars": [
+      "error",
+      {
+        "varsIgnorePattern": "should|expect"
+      }
+    ]
+  },
+  overrides: [
+    {
+        files: "*.spec.js",
+        rules: {
+            "no-unused-expressions": "off"
+        }
+    }
+]
 };
